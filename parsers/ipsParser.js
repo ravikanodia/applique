3bc89407edf6f33c5158da03228e1b02166aa38e
Ravi Kanodia <ravileonkanodia@gmail.com>
2017-02-22 23:45:27 -0800

    Moved file functions to a separate module

--- parsers/ipsParser.js
+++ parsers/ipsParser.js
@@ -1,4 +1,5 @@
 const fs = require('fs');
+const File = require('../lib/File');
 
 // Based on http://zerosoft.zophar.net/ips.php
 
@@ -53,19 +54,6 @@ var IpsParser = function(filename) {
     this.filename = filename;
     this.file = null;
 
-    // TODO: Move filesystem-related functions to a separate module.
-    // Returns a new buffer of size length, filled with data from the file.
-    // If not enough data was read, throws an error.
-    this.readBytesAsBuffer = function(length, name, position=null) {
-	var buf = Buffer.alloc(length);
-	var bytesRead = fs.readSync(this.file, buf, 0, length, position);
-	if (bytesRead != length) {
-	    throw new Error("Expected " + length + " bytes of " + name + " but hit end of file after " + bytesRead + " bytes.");
-	}
-        console.dir("Got " + length + " bytes of " + name);
-	return buf;
-    };
-
     // Doing this in the filesystem avoids having to fit a potentially
     // large Buffer in memory all at once. Also, node Buffers can't be resized
     // after they are created, and IPS patches can write past the end of the
@@ -85,7 +73,7 @@ var IpsParser = function(filename) {
 
     this.validateIpsHeader = function() {
 	var headerBuffer =
-	    this.readBytesAsBuffer(IPS_HEADER.length, "header");
+	    File.readBytesAsBuffer(this.file, IPS_HEADER.length, "header");
 	if (headerBuffer != IPS_HEADER) {
 	    throw new Error("Invalid IPS patch header");
 	}
@@ -97,7 +85,7 @@ var IpsParser = function(filename) {
 	// don't just stop when you see "EOF" in the offset field - make sure
 	// this is the end of the file. If not, the IPS patch is malformed.
 	var offsetBuffer =
-	    this.readBytesAsBuffer(IPS_OFFSET_SIZE, "offset or EOF");
+	    File.readBytesAsBuffer(this.file, IPS_OFFSET_SIZE, "offset or EOF");
 
 	var sizeBuf = Buffer.alloc(IPS_SIZE_SIZE);
 	var sizeBytesRead = fs.readSync(
@@ -116,19 +104,21 @@ var IpsParser = function(filename) {
 	var size = sizeBuf.readUIntBE(0, IPS_SIZE_SIZE);
 	if (size == 0) {
 	    console.dir("size field is 0 (record is run-length encoding)");
-	    var runLengthBuffer = this.readBytesAsBuffer(IPS_RLE_LENGTH_SIZE, "run length");
+	    var runLengthBuffer =
+		File.readBytesAsBuffer(this.file, IPS_RLE_LENGTH_SIZE, "run length");
 	    var runLength = runLengthBuffer.readUIntBE(0, 2);
 	    if (runLength === 0) {
 		throw new Error("Can't have a run of zero length.");
 	    }
 
-	    var valueBuffer = this.readBytesAsBuffer(IPS_RLE_VALUE_SIZE, "run value");
+	    var valueBuffer =
+		File.readBytesAsBuffer(this.file, IPS_RLE_VALUE_SIZE, "run value");
 
 	    console.dir("RLE length: " + runLength + "; data: " + valueBuffer)
 	    return this.makeRlePatch(offset, runLength, valueBuffer);
 	} else {
 	    console.dir("size is " + size + " (plain data)");
-	    var dataBuffer = this.readBytesAsBuffer(size, "data");
+	    var dataBuffer = File.readBytesAsBuffer(this.file, size, "data");
 	    return this.makeDataPatch(offset, dataBuffer);
 	}	
     }
