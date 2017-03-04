2f680a66a6a7dda325c0d3c1f2a02946d2691970
Ravi Kanodia <ravileonkanodia@gmail.com>
2017-03-03 22:16:04 -0800

    just one buffer classfor reading and writing

--- index.js
+++ index.js
@@ -5,54 +5,46 @@ const fs = require('fs');
 var ArgumentParser = require('argparse').ArgumentParser;
 var ipsParser = require('./parsers/ipsParser');
 var upsParser = require('./parsers/upsParser');
-var patcher = require('./patcher');
-var File = require('./lib/File');
 var SourceBuffer = require('./lib/SourceBuffer');
-var OutputBuffer = require('./lib/OutputBuffer');
 
-var parser = new ArgumentParser({
+var argParser = new ArgumentParser({
 	version: '0.0.0',
 	addHelp: true,
         description: 'Apply an IPS patch to a file.'
     });
-parser.addArgument(
+argParser.addArgument(
     ['-p', '--patch'],
     {
 	help: 'patch file'
     });
-parser.addArgument(
+argParser.addArgument(
     ['-f', '--file'],
     {
 	help: 'base file'
     });
-parser.addArgument(
+argParser.addArgument(
     ['-o', '--output'],
     {
 	help: 'output file'
     });
-parser.addArgument(
+argParser.addArgument(
     ['-t', '--type'],
     {
 	choices: ['ips', 'ups'],
 	help: 'patch type',
     });
-parser.addArgument(
+argParser.addArgument(
     ['-d', '--dry-run'],
     {
 	help: 'dry run (don\'t save output file)'
     });
 
-var args = parser.parseArgs();
-
-//var inputFile = File(args.file);
-//var patchFile = File(args.patch);
-//console.log("input filesize is: " + inputFile.getFileLength());
-//console.log("patch filesize is: " + patchFile.getFileLength());
+var args = argParser.parseArgs();
 
 var parser;
 var inputSource = SourceBuffer(fs.readFileSync(args.file));
 var patchSource = SourceBuffer(fs.readFileSync(args.patch));
-var outputBuffer = OutputBuffer();
+var outputBuffer = SourceBuffer();
 if (args.type == 'ups') {
     parser = upsParser(inputSource, patchSource, outputBuffer);
 } else {    
@@ -61,4 +53,3 @@ if (args.type == 'ups') {
 parser.applyAllPatches();
 
 outputBuffer.saveToFile(args.output);
-
