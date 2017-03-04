8e04646fdcc9a3a31fd22a2b105c4a86260082a9
Ravi Kanodia <ravileonkanodia@gmail.com>
2017-03-03 23:55:58 -0800

    adds some basic tests for SourceBuffer

---
+++ test/SourceBuffer_test.js
@@ -0,0 +1,54 @@
+const SourceBuffer = require('../lib/SourceBuffer');
+
+describe('SourceBuffer', function() {
+    describe('when initialized', function() {
+	it('stores a buffer', function(done) {
+	    var sb = SourceBuffer(Buffer("abcdefg"));
+	    sb.getLength().should.equal(7);
+	    sb.readBytesAsBuffer(1)[0].should.equal('a'.charCodeAt(0));
+
+	    done();
+	});
+
+	it('is empty no buffer was passed in', function(done) {
+	    var sb = SourceBuffer();
+	    sb.getLength().should.equal(0);
+
+	    done();
+	});
+    });
+
+    describe('comparison', function() {
+	it('returns true if the other buffer holds the same content', function(done) {
+	    var aBuf = SourceBuffer(Buffer("abcdefg"));
+	    var bBuf = SourceBuffer(Buffer("abcdefg"));
+
+	    aBuf.equals(bBuf).should.equal(true);
+	    done();
+	});
+
+	it('returns false if the other buffer holds differing content', function(done) {
+    	    var aBuf = SourceBuffer(Buffer("abcdefg"));
+	    var bBuf = SourceBuffer(Buffer("gfedcba"));
+
+	    aBuf.equals(bBuf).should.equal(false);
+	    done();
+	});
+
+	it('returns false if the other buffer is shorter', function(done) {
+    	    var aBuf = SourceBuffer(Buffer("abcdefg"));
+	    var bBuf = SourceBuffer(Buffer("abcde"));
+
+	    aBuf.equals(bBuf).should.equal(false);
+	    done();
+	});
+
+	it('returns false if the other buffer is longer', function(done) {
+    	    var aBuf = SourceBuffer(Buffer("abcdefg"));
+	    var bBuf = SourceBuffer(Buffer("abcdefghi"));
+
+	    aBuf.equals(bBuf).should.equal(false);
+	    done();
+	});
+    });
+});
